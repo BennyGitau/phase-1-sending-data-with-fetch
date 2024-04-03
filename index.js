ccc9742085d76f77c258d48acc7e5d2b6bd1088a
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-03 16:52:21 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  })
+
+})
+  .then(response => response.json())
+  .then(dog => console.log(dog))
+
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        })
+        
+    })
+    .then(response => response.json())
+    .then((user) => {
+        const p = document.createElement("p")
+        p.textContent = user.id;
+        document.querySelector("body").appendChild(p);
+    })
+    .catch((error) => {
+        if(error) {
+        const p = document.createElement("p")
+        p.textContent = "Unauthorized Access"
+        document.querySelector("body").appendChild(p);
+        }
+    })
+
+}
+submitData("Benny", "Benny@gmail.com")
